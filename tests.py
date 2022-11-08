c252ad9fb1d7f0d33c507778ef8da4918b1fbf85
StaBdv <s.badov@gmail.com>
2022-11-08 12:21:11 +0300

    Исправила book_raiting.get на get_book_rating

--- tests.py
+++ tests.py
@@ -1,42 +1,36 @@
 from main import BooksCollector
 
 class TestBooksCollector:
-
     def test_add_new_book_add_rating(self):
         collector = BooksCollector()
         collector.add_new_book('Гордость и предубеждение и зомби')
-        assert collector.books_rating.get('Гордость и предубеждение и зомби') == 1
-
+        assert collector.get_book_rating('Гордость и предубеждение и зомби') == 1
     def test_add_new_book_add_two_books(self):
         collector = BooksCollector()
         collector.add_new_book('Гордость и предубеждение и зомби')
         collector.add_new_book('Что делать, если ваш кот хочет вас убить')
         assert len(collector.get_books_rating()) == 2
-
     def test_add_new_book_add_not_uniq_books(self):
         collector = BooksCollector()
         collector.add_new_book('Гордость и предубеждение и зомби')
         collector.add_new_book('Гордость и предубеждение и зомби')
-        assert len(collector.get_books_rating()) == 1
-
+        books_rating = collector.get_books_rating()
+        assert len(books_rating) == 1
     def test_set_book_rating(self):
         collector = BooksCollector()
         collector.add_new_book('Гордость и предубеждение и зомби')
         collector.set_book_rating('Гордость и предубеждение и зомби', 5)
-        assert collector.books_rating.get('Гордость и предубеждение и зомби') == 5
-
+        assert collector.get_book_rating('Гордость и предубеждение и зомби') == 5
     def test_test_set_book_rating_0(self):
         collector = BooksCollector()
         collector.add_new_book('Гордость и предубеждение и зомби')
         collector.set_book_rating('Гордость и предубеждение и зомби', 0)
-        assert collector.books_rating.get('Гордость и предубеждение и зомби') == 1
-
+        assert collector.get_book_rating('Гордость и предубеждение и зомби') == 1
     def test_set_book_rating_over_10(self):
         collector = BooksCollector()
         collector.add_new_book('Гордость и предубеждение и зомби')
         collector.set_book_rating('Гордость и предубеждение и зомби', 11)
-        assert collector.books_rating.get('Гордость и предубеждение и зомби') == 1
-
+        assert collector.get_book_rating('Гордость и предубеждение и зомби') == 1
     def test_get_books_with_specific_rating(self):
         collector = BooksCollector()
         collector.add_new_book('Гордость и предубеждение и зомби')
@@ -46,7 +40,6 @@ class TestBooksCollector:
         collector.add_new_book('Metro')
         collector.set_book_rating('Metro', 3)
         assert collector.get_books_with_specific_rating(3) == ['Что делать, если ваш кот хочет вас убить', 'Metro']
-
     def test_add_book_in_favorites(self):
         collector = BooksCollector()
         collector.add_new_book('Гордость и предубеждение и зомби')
@@ -54,7 +47,6 @@ class TestBooksCollector:
         collector.add_book_in_favorites('Гордость и предубеждение и зомби')
         collector.add_book_in_favorites('Что делать, если ваш кот хочет вас убить')
         assert collector.get_list_of_favorites_books() == ['Гордость и предубеждение и зомби', 'Что делать, если ваш кот хочет вас убить']
-
     def test_add_book_in_favorites_no_doubles(self):
         collector = BooksCollector()
         collector.add_new_book('Гордость и предубеждение и зомби')
@@ -63,16 +55,13 @@ class TestBooksCollector:
         collector.add_book_in_favorites('Гордость и предубеждение и зомби')
         collector.add_book_in_favorites('Что делать, если ваш кот хочет вас убить')
         assert collector.get_list_of_favorites_books() == ['Гордость и предубеждение и зомби', 'Что делать, если ваш кот хочет вас убить']
-
     def test_add_book_in_favorites_out_of_list(self):
         collector = BooksCollector()
         collector.add_book_in_favorites('Гордость и предубеждение и зомби')
         assert len(collector.get_list_of_favorites_books()) == 0
-
     def test_delete_book_from_favorites(self):
         collector = BooksCollector()
         collector.add_new_book('Гордость и предубеждение и зомби')
         collector.add_book_in_favorites('Гордость и предубеждение и зомби')
         collector.delete_book_from_favorites('Гордость и предубеждение и зомби')
         assert len(collector.get_list_of_favorites_books()) == 0
-
